fd25144d8646921298e1d6112707535d24aa04c9
mahbubul14 <mahbub21alam2017@gmail.com>
2022-05-03 05:58:16 +0600

    Add 3 cards on mainpage

--- src/components/itemCard/itemCard.js
+++ src/components/itemCard/itemCard.js
@@ -1,11 +1,27 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 const ItemCard = () => (
-  <div class="bg-red-500">
-    ItemCard
-
-    <edp>this is title for card</edp>
-  </div>
+  <>
+    <div>
+      <h1>LATEST MODELS</h1>
+      <h4>Please select a Tesla Model</h4>
+    </div>
+    <section class="grid grid-cols-3">
+      <div class="bg-red-500 rounded-full">
+        <h1>Model S</h1>
+        <p>The Tesla Model S is a battery-powered liftback car produced by Tesla, Inc. It was introduced on June 22, 2012, and is positioned as Tesla&apos;s flagship model.</p>
+      </div>
+      <div class="bg-blue-500">
+        <h1>Model S</h1>
+        <p>The Tesla Model S is a battery-powered liftback car produced by Tesla, Inc. It was introduced on June 22, 2012, and is positioned as Tesla&apos;s flagship model.</p>
+      </div>
+      <div class="bg-green-500">
+        <h1>Model S</h1>
+        <p>The Tesla Model S is a battery-powered liftback car produced by Tesla, Inc. It was introduced on June 22, 2012, and is positioned as Tesla&apos;s flagship model.</p>
+      </div>
+    </section>
+  </>
 );
 
 export default ItemCard;
