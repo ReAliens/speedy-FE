caadc3446222619683466387a7f54857d10d23bd
Mucyosoda <mucyosofa@gmail.com>
2022-05-03 14:01:14 +0200

    linter error

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Cards(prop) {
   return (
     <div>
+      /* eslint-disable react/destructuring-assignment */
       <img src={prop.image} alt="moto" />
       <h3>{prop.title}</h3>
       <p>{prop.description}</p>
