5b78f6b34adfab41da676bf1b38814432e14a872
mahbubul14 <mahbub21alam2017@gmail.com>
2022-05-09 22:37:03 +0600

    Update form headline

--- src/pages/reservationsPage/Reservations.jsx
+++ src/pages/reservationsPage/Reservations.jsx
@@ -23,16 +23,14 @@ const Reservations = () => {
             <h2 className="mt-8 text-2xl">Make Your Reservation</h2>
             <h5 className="text-fuchsia-700 mt-10">Select Car</h5>
             <input type="text" className="border-2 mt-1 mb-6 " />
-            <h5 className="text-fuchsia-700">Model</h5>
+            <h5 className="text-fuchsia-700">City</h5>
             <input type="text" className="border-2 mt-1 mb-6" />
-            <h5 className="text-fuchsia-700">Price</h5>
+            <h5 className="text-fuchsia-700">Start Date</h5>
             <input type="text" className="border-2 mt-1 mb-6" />
-            <h5 className="text-fuchsia-700">Image</h5>
-            <input type="text" className="border-2 mt-1 mb-6" />
-            <h5 className="text-fuchsia-700">Description</h5>
+            <h5 className="text-fuchsia-700">End Date</h5>
             <input type="text" className="border-2 mt-1 mb-6" />
             <br />
-            <input type="submit" className="border-2 mt-5 mb-10 w-40 cursor-pointer" />
+            <input type="submit" className="bg-slate-300 p-2 w- border-2 mt-5 mb-10 w-64 cursor-pointer" />
 
           </form>
         </div>
