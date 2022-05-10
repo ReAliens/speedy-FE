c5770e97b4ba33455af1b95ca881986991ec6497
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-05-10 20:03:31 +0200

    Fix conflict

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -3,56 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { logoutAction } from '../../redux/auth/authActions';
 
-<<<<<<< HEAD
-const Sidebar = ({ isOpen }) => isOpen && (
-<div className="overflow-y-auto py-4 h-full px-3 bg-gray-50 rounded dark:bg-gray-800 fixed top-0 left-0 pt-16 w-[25vw]">
-  <ul className="space-y-2">
-    <li>
-      <a
-        href="/"
-        className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-      >
-        <span className="ml-3">Cars List</span>
-      </a>
-    </li>
-    <li>
-      <a
-        href="/reservations"
-        className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-      >
-        <span className="flex-1 ml-3 whitespace-nowrap">Reserve Car</span>
-      </a>
-    </li>
-    <li>
-      <a
-        href="/"
-        className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-      >
-        <span className="flex-1 ml-3 whitespace-nowrap">
-          My Reservations
-        </span>
-      </a>
-    </li>
-    <li>
-      <a
-        href="/"
-        className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-      >
-        <span className="flex-1 ml-3 whitespace-nowrap">Add new Car</span>
-      </a>
-    </li>
-    <li>
-      <a
-        href="/"
-        className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-      >
-        <span className="flex-1 ml-3 whitespace-nowrap">Delete Car</span>
-      </a>
-    </li>
-  </ul>
-</div>
-);
-=======
 const Sidebar = ({ isOpen }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -152,6 +102,5 @@ const Sidebar = ({ isOpen }) => {
     )
   );
 };
->>>>>>> 18305fd3308f25125009b76f979a8bb23265e1e9
 
 export default Sidebar;
