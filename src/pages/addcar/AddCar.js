02b0eb7f6b549834c1c2650df7c868f13dd6e646
mahbubul14 <mahbub21alam2017@gmail.com>
2022-05-10 06:26:13 +0600

    Update design

--- src/pages/addcar/AddCar.js
+++ src/pages/addcar/AddCar.js
@@ -17,22 +17,22 @@ const AddCar = () => {
   return (
     <main>
       <section className="flex justify-center">
-        <div className="font-medium bg-violet-50 mt-24 flex justify-center w-1/2">
+        <div className="font-medium bg-violet-50 mt-24 flex justify-center p-24">
 
           <form onSubmit={handleSubmit(submit)}>
-            <h2 className="mt-8 text-2xl">Add Car to Our Collection</h2>
-            <h5 className="text-fuchsia-700 mt-10">Brand</h5>
-            <input type="text" className="border-2 mt-1 mb-6 " placeholder="eg: Porsche" />
-            <h5 className="text-fuchsia-700">Model</h5>
-            <input type="text" className="border-2 mt-1 mb-6" placeholder="eg: 718 Boxster" />
-            <h5 className="text-fuchsia-700">Price</h5>
-            <input type="text" className="border-2 mt-1 mb-6" placeholder="eg: $62,600" />
-            <h5 className="text-fuchsia-700">Image</h5>
-            <input type="text" className="border-2 mt-1 mb-6" placeholder="Image URL" />
-            <h5 className="text-fuchsia-700">Description</h5>
-            <input type="text" className="border-2 mt-1 mb-6" placeholder="Car Description" />
+            <h2 className="underline underline-offset-8 decoration-fuchsia-700 decoration-double mt-8 mb-16 text-3xl">Add Car to Our Collection</h2>
+            <h5 className="text-xl text-fuchsia-700 mt-10">Brand</h5>
+            <input type="text" className="h-10 w-72 border-2 mt-1 mb-6" placeholder="eg: Porsche" />
+            <h5 className="text-xl text-fuchsia-700">Model</h5>
+            <input type="text" className="h-10 w-72 border-2 mt-1 mb-6" placeholder="eg: 718 Boxster" />
+            <h5 className="text-xl text-fuchsia-700">Price</h5>
+            <input type="text" className="h-10 w-72 border-2 mt-1 mb-6" placeholder="eg: $62,600" />
+            <h5 className="text-xl text-fuchsia-700">Image</h5>
+            <input type="text" className="h-10 w-72 border-2 mt-1 mb-6" placeholder="Image URL" />
+            <h5 className="text-xl text-fuchsia-700">Description</h5>
+            <input type="text" className="h-10 w-72 border-2 mt-1 mb-6" placeholder="Car Description" />
             <br />
-            <input type="submit" className="border-2 mt-5 mb-10 w-40 cursor-pointer" />
+            <input type="submit" className="text-2xl font-bold text-fuchsia-700 border-fuchsia-700 p-2 w- border-4 mt-8 w-72 cursor-pointer" />
 
           </form>
         </div>
