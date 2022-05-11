fb25df603e6aa07543f13055492c154c40744622
mahbubul14 <mahbub21alam2017@gmail.com>
2022-05-11 19:13:05 +0600

    Update styling on add car page

--- src/pages/addcar/AddCar.jsx
+++ src/pages/addcar/AddCar.jsx
@@ -19,15 +19,15 @@ const AddCar = () => {
       <section className="flex justify-center">
         <div className="bg-slate-100 mt-24">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <h2 className="shrink text-slate-50 font-bold font-serif flex justify-center border border-red-800 mb-8 text-xl py-2 bg-purple-700 w-full">
+            <h2 className="text-slate-50 font-bold font-serif flex justify-center border border-red-800 mb-8 text-xl py-2 bg-purple-700 w-full md:h-16 md:text-3xl">
               Add Car to Our Collection
             </h2>
-            <div class="text-base font-medium mx-10 mb-10">
+            <div class="text-xs font-normal mx-10 mb-10 md:px-20 sm:text-base lg:text-lg">
             <h5 className="text-fuchsia-700">Brand</h5>
             <input
               {...register('name', { required: true })}
               type="text"
-              className="h-7 w-60 border-2"
+              className="h-7 w-60 border-2 md:w-auto"
               placeholder="eg: Porsche"
             />
             <p className="text-red-500 mb-6">
@@ -37,7 +37,7 @@ const AddCar = () => {
             <input
               {...register('specs', { required: true })}
               type="text"
-              className="h-7 w-60 border-2"
+              className="h-7 w-60 border-2 md:w-auto"
               placeholder="eg: 718 Boxster"
             />
             <p className="text-red-500 mb-6">
@@ -47,7 +47,7 @@ const AddCar = () => {
             <input
               {...register('price', { required: true })}
               type="text"
-              className="h-7 w-60 border-2"
+              className="h-7 w-60 border-2 md:w-auto"
               placeholder="eg: $62,600"
             />
             <p className="text-red-500 mb-6">
@@ -57,7 +57,7 @@ const AddCar = () => {
             <input
               {...register('image', { required: true })}
               type="text"
-              className="h-7 w-60 border-2"
+              className="h-7 w-60 border-2 md:w-auto"
               placeholder="Image URL"
             />
             <p className="text-red-500 mb-6">
@@ -67,7 +67,7 @@ const AddCar = () => {
             <textarea
               {...register('description', { required: true })}
               type="text"
-              className="h-24 w-60 border-2 "
+              className="h-24 w-60 border-2 md:w-64 md:h-32"
               placeholder="Car Description"
             />
             <p className="text-red-500">
@@ -76,7 +76,7 @@ const AddCar = () => {
             <br />
             <input
               type="submit"
-              className="font-bold text-fuchsia-700 border-fuchsia-700 py-px border-2 w-28 cursor-pointer"
+              className="uppercase font-serif font-bold text-fuchsia-700 border-fuchsia-700 py-px border-2 w-20 cursor-pointer md:ml-12 md:py-1 md:w-40 md:text-base md:bg-purple-700 md:text-white md:font-semi-bold"
             />
             </div>
           </form>
