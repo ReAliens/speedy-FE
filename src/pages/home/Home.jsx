9810a17aede3b3107276bf6725ed1450eae6b1ec
Mucyosoda <mucyosofa@gmail.com>
2022-05-11 11:20:18 +0200

    add style for inputs

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,21 +8,21 @@ const Home = () => (
       alt="page-cover"
       className="object-contain w-full h-full"
     />
-    <div className="absolute top-[10%] h-[70vh] text-center flex flex-col justify-around items-center py-10">
+    <div className="absolute top-[8%] h-[60vh] flex flex-col justify-around items-center py-2 pl-12">
       <div className="text-white text-center font-bold text-6xl flex w-full items-center justify-center">
         <span>Speedy</span>
       </div>
-      <p className="text-white opacity-75 font-bold text-2xl mt-4">
+      <p className="text-white opacity-75 font-bold text-2xl mt-0">
         Pick your favorite car in just one click
       </p>
-      <div className="flex w-full justify-around">
+      <div className="flex w-full justify-around pt-12">
         <Link to="/login">
-          <span className="border-2 border-green-500 rounded-3xl hover:text-green-500 hover:bg-white hover:font-bold py-2 px-10 bg-green-500  text-white">
+          <span className=" rounded-full hover:text-green-500 hover:bg-white hover:font-bold py-6 px-2 bg-orange-500 mr-52 font-bold text-white">
             Log in
           </span>
         </Link>
         <Link to="/signup">
-          <span className="border-2 border-green-500 rounded-3xl hover:text-green-500 hover:bg-white hover:font-bold py-2 px-10 bg-green-500  text-white">
+          <span className="rounded-full hover:text-green-500 hover:bg-white hover:font-bold  bg-orange-500 mr-10 py-5 font-bold text-white">
             Sign up
           </span>
         </Link>
