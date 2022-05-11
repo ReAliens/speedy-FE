56bd5f53d4bdafc3578e5ac4dd1511afae139e8d
mahbubul14 <mahbub21alam2017@gmail.com>
2022-05-11 09:50:59 +0600

    Update design for mobile version

--- src/pages/addcar/AddCar.jsx
+++ src/pages/addcar/AddCar.jsx
@@ -17,66 +17,68 @@ const AddCar = () => {
   return (
     <main>
       <section className="flex justify-center">
-        <div className="font-medium bg-violet-50 mt-24 flex justify-center p-24">
+        <div className="bg-slate-100 mt-24">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <h2 className="underline underline-offset-8 decoration-fuchsia-700 decoration-double mt-8 mb-16 text-3xl">
+            <h2 className="shrink text-slate-50 font-bold font-serif flex justify-center border border-red-800 mb-8 text-xl py-2 bg-purple-700 w-full">
               Add Car to Our Collection
             </h2>
-            <h5 className="text-xl text-fuchsia-700 mt-10">Brand</h5>
+            <div class="text-base font-medium mx-10 mb-10">
+            <h5 className="text-fuchsia-700">Brand</h5>
             <input
               {...register('name', { required: true })}
               type="text"
-              className="h-10 w-72 border-2 mt-1 "
+              className="h-7 w-60 border-2"
               placeholder="eg: Porsche"
             />
             <p className="text-red-500 mb-6">
               {errors.name && 'this field is required'}
             </p>
-            <h5 className="text-xl text-fuchsia-700">Model</h5>
+            <h5 className="text-fuchsia-700">Model</h5>
             <input
               {...register('specs', { required: true })}
               type="text"
-              className="h-10 w-72 border-2 mt-1"
+              className="h-7 w-60 border-2"
               placeholder="eg: 718 Boxster"
             />
             <p className="text-red-500 mb-6">
               {errors.specs && 'this field is required'}
             </p>
-            <h5 className="text-xl text-fuchsia-700">Price</h5>
+            <h5 className="text-fuchsia-700">Price</h5>
             <input
               {...register('price', { required: true })}
               type="text"
-              className="h-10 w-72 border-2 mt-1 "
+              className="h-7 w-60 border-2"
               placeholder="eg: $62,600"
             />
             <p className="text-red-500 mb-6">
               {errors.price && 'this field is required'}
             </p>
-            <h5 className="text-xl text-fuchsia-700">Image</h5>
+            <h5 className="text-fuchsia-700">Image</h5>
             <input
               {...register('image', { required: true })}
               type="text"
-              className="h-10 w-72 border-2 mt-1 "
+              className="h-7 w-60 border-2"
               placeholder="Image URL"
             />
             <p className="text-red-500 mb-6">
               {errors.image && 'this field is required'}
             </p>
-            <h5 className="text-xl text-fuchsia-700">Description</h5>
-            <input
+            <h5 className="text-fuchsia-700">Description</h5>
+            <textarea
               {...register('description', { required: true })}
               type="text"
-              className="h-10 w-72 border-2 mt-1"
+              className="h-24 w-60 border-2 "
               placeholder="Car Description"
             />
-            <p className="text-red-500 mb-6">
+            <p className="text-red-500">
               {errors.description && 'this field is required'}
             </p>
             <br />
             <input
               type="submit"
-              className="text-2xl font-bold text-fuchsia-700 border-fuchsia-700 p-2 w- border-4 mt-8 w-72 cursor-pointer"
+              className="font-bold text-fuchsia-700 border-fuchsia-700 py-px border-2 w-28 cursor-pointer"
             />
+            </div>
           </form>
         </div>
       </section>
