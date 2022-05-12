9b2dbf5b6ea4a71b7a417d012c627468e4902736
Ahmed <ahmedreda152@gmail.com>
2022-05-12 02:58:23 +0200

    fix add car form responsive

--- src/pages/addcar/AddCar.jsx
+++ src/pages/addcar/AddCar.jsx
@@ -21,76 +21,77 @@ const AddCar = () => {
   };
 
   return (
-    <main>
-      <section className="flex justify-center">
-        <div className="bg-slate-100 mt-24 border-2 border-green-900">
+    <main className="flex justify-center items-center w-full h-[95vh]">
+      <section className="flex w-[90%] md:px-28">
+        <div className="bg-slate-100 border-2 border-green-900 w-full">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <h2 className="text-slate-50 font-bold font-serif flex justify-center mb-8 text-xl py-2 bg-green-700 w-full md:h-16 md:text-3xl">
+            <h2 className="text-slate-50 font-bold font-serif flex justify-center mb-8 text-xl py-2 bg-green-600 w-full md:h-16 md:text-3xl">
               Add Car to Our Collection
             </h2>
-            <div class="text-xs font-normal px-10 mb-10 md:px-20 sm:text-base lg:text-lg">
-            <h5 className="text-fuchsia-700">Brand</h5>
-            <input
-              {...register('name', { required: true })}
-              type="text"
-              className="p-2 md:p-4  h-7 w-60 border-2 md:w-auto"
-              placeholder="eg: Porsche"
-            />
-            <p className="text-red-500 mb-6">
-              {errors.name && 'this field is required'}
-            </p>
-            <h5 className="text-fuchsia-700">Model</h5>
-            <input
-              {...register('specs', { required: true })}
-              type="text"
-              className="p-2 md:p-4 h-7 w-60 border-2 md:w-auto"
-              placeholder="eg: 718 Boxster"
-            />
-            <p className="text-red-500 mb-6">
-              {errors.specs && 'this field is required'}
-            </p>
-            <h5 className="text-fuchsia-700">Price</h5>
-            <input
-              {...register('price', { required: true })}
-              type="text"
-              className="p-2 md:p-4 h-7 w-60 border-2 md:w-auto"
-              placeholder="eg: $62,600"
-            />
-            <p className="text-red-500 mb-6">
-              {errors.price && 'this field is required'}
-            </p>
-            <h5 className="text-fuchsia-700">Image</h5>
-            <input
-              {...register('photo', { required: true })}
-              type="text"
-              className="p-2 md:p-4 h-7 w-60 border-2 md:w-auto"
-              placeholder="Image URL"
-            />
+            <div className="text-xs font-normal px-10 mb-10 md:px-20 sm:text-base lg:text-lg">
+              <h5 className="text-fuchsia-700">Brand</h5>
+              <input
+                {...register('name', { required: true })}
+                type="text"
+                className="p-2 md:p-4  h-7 w-full border-2"
+                placeholder="eg: Porsche"
+              />
+              <p className="text-red-500 mb-6">
+                {errors.name && 'this field is required'}
+              </p>
+              <h5 className="text-fuchsia-700">Model</h5>
+              <input
+                {...register('specs', { required: true })}
+                type="text"
+                className="p-2 md:p-4 h-7 w-full border-2"
+                placeholder="eg: 718 Boxster"
+              />
+              <p className="text-red-500 mb-6">
+                {errors.specs && 'this field is required'}
+              </p>
+              <h5 className="text-fuchsia-700">Price</h5>
+              <input
+                {...register('price', { required: true })}
+                type="text"
+                className="p-2 md:p-4 h-7 w-full border-2"
+                placeholder="eg: $62,600"
+              />
+              <p className="text-red-500 mb-6">
+                {errors.price && 'this field is required'}
+              </p>
+              <h5 className="text-fuchsia-700">Image</h5>
+              <input
+                {...register('photo', { required: true })}
+                type="text"
+                className="p-2 md:p-4 h-7 w-full border-2"
+                placeholder="Image URL"
+              />
 
-            <p className="text-red-500 mb-6">
-              {errors.image && 'this field is required'}
-            </p>
-            <h5 className="text-fuchsia-700">Description</h5>
-            <textarea
-              {...register('description', { required: true })}
-              type="text"
-              className="p-2 md:p-4 h-24 w-60 border-2 md:w-72 md:h-32"
-              placeholder="Car Description"
-            />
-            <p className="text-red-500">
-              {errors.description && 'this field is required'}
-            </p>
-            <input
-              {...register('user_id', { required: true })}
-              type="hidden"
-              value={currentUser.auth.data.id}
-            />
-            <br />
-
-            <input
-              type="submit"
-              className="bg-green-50 rounded uppercase font-serif font-bold text-fuchsia-700 border-green-700 py-1 px-6 border-2 cursor-pointer md:ml-14 md:py-1 md:w-36 md:text-base md:bg-green-700 md:text-white md:font-semi-bold md:border-green-900 md:rounded-full"
-            />
+              <p className="text-red-500 mb-6">
+                {errors.photo && 'this field is required'}
+              </p>
+              <h5 className="text-fuchsia-700">Description</h5>
+              <textarea
+                {...register('description', { required: true })}
+                type="text"
+                className="p-2 md:p-4 h-24 w-full border-2  md:h-32"
+                placeholder="Car Description"
+              />
+              <p className="text-red-500">
+                {errors.description && 'this field is required'}
+              </p>
+              <input
+                {...register('user_id', { required: true })}
+                type="hidden"
+                value={currentUser.auth.data.id}
+              />
+              <br />
+              <div className="flex w-full justify-center">
+                <input
+                  type="submit"
+                  className="bg-green-50 hover:text-green-700 hover:bg-white rounded uppercase font-serif font-bold text-fuchsia-700 border-green-700 py-1 px-6 border-2 cursor-pointer md:ml-14 md:py-1 md:w-36 md:text-base md:bg-green-600 md:text-white md:font-semi-bold md:border-green-900 md:rounded-full"
+                />
+              </div>
             </div>
           </form>
         </div>
