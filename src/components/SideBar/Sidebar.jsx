6d87ed32c49cbd8e1e27f0c994d9d94454a8b3e8
Ahmed <ahmedreda152@gmail.com>
2022-05-12 16:30:39 +0200

    little touches

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -13,52 +13,29 @@ const Sidebar = ({ isOpen }) => {
     dispatch(logoutAction());
     navigate('/');
   };
+  const token = localStorage.getItem('token');
+
   return (
     isOpen && (
-      <div className=" side-bar overflow-y-auto py-4 h-full px-3 bg-gray-50 rounded dark:bg-gray-800 fixed top-0 left-0 pt-16 w-[17vw] ">
+      <div className=" side-bar overflow-y-auto py-4 h-full px-3 bg-gray-50 rounded dark:bg-gray-800 fixed top-0 left-0 pt-16 w-[100vw] sm:w-[20vw] ">
         <ul className="space-y-2">
-          <li>
-            <Link
-              to={selector.data ? '/' : '/home'}
-              className=" side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 "
-            >
-              <span className="ml-3">Cars List</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/"
-              className=" side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="ml-3">Home</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/login"
-              className=" side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span>Log in</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/signup"
-              className="side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span>Sign up</span>
-            </Link>
-          </li>
-          {selector.data && (
+          {selector.data && token ? (
             <>
+              <li>
+                <Link
+                  to={selector.data ? '/' : '/home'}
+                  className=" side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 "
+                >
+                  <span className="flex-1 whitespace-nowrap">Cars List</span>
+                </Link>
+              </li>
+              <li></li>
               <li>
                 <a
                   href="/add-reservation"
                   className="side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                  <span className="flex-1 ml-3 whitespace-nowrap">
-                    Reserve Car
-                  </span>
+                  <span className="flex-1 whitespace-nowrap">Reserve Car</span>
                 </a>
               </li>
               <li>
@@ -66,9 +43,7 @@ const Sidebar = ({ isOpen }) => {
                   href="/reservations"
                   className="side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                  <span className="flex-1 ml-3 whitespace-nowrap">
-                    My Reservations
-                  </span>
+                  <span className="flex-1 whitespace-nowrap">Reservations</span>
                 </a>
               </li>
               <li>
@@ -76,9 +51,7 @@ const Sidebar = ({ isOpen }) => {
                   href="/add-car"
                   className="side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                  <span className="flex-1 ml-3 whitespace-nowrap">
-                    Add new Car
-                  </span>
+                  <span className="flex-1  whitespace-nowrap">Add new Car</span>
                 </a>
               </li>
               <li>
@@ -86,33 +59,55 @@ const Sidebar = ({ isOpen }) => {
                   href="/delete-car"
                   className="side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
-                  <span className="flex-1 ml-3 whitespace-nowrap">
-                    Delete Car
-                  </span>
+                  <span className="flex-1  whitespace-nowrap">Delete Car</span>
                 </a>
               </li>
               <li>
                 <button
                   type="button"
                   onClick={() => logout()}
-                  className="side-bar-link logout"
+                  className="side-bar-link logout w-full text-left"
                 >
                   Log out
                 </button>
               </li>
             </>
+          ) : (
+            <>
+              <li>
+                <Link
+                  to={selector.data ? '/' : '/home'}
+                  className=" side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 "
+                >
+                  <span>Cars List</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className=" side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <span>Home</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/login"
+                  className=" side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <span>Log in</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/signup"
+                  className="side-bar-link flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <span>Sign up</span>
+                </Link>
+              </li>
+            </>
           )}
-          <ul className="social-media-list">
-            <li className="social-media">
-              <img src="./images/facebook.svg" alt="facebook" />
-            </li>
-            <li className="social-media">
-              <img src="./images/twitter.svg" alt="twitter" />
-            </li>
-            <li className="social-media">
-              <img src="./images/instagram.svg" alt="instagram" />
-            </li>
-          </ul>
         </ul>
       </div>
     )
